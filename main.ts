fd205353bf8e243593697c523f197258ea28726b
Alejandro G <agomcue486@gmail.com>
2023-05-30 14:25:31 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,54 @@
+ControllerButtonEvent.Pressed.onEvent(controller.right, function () {
+    truck.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 2 2 2 2 2 2 2 2 . . . . 
+        . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+        . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+        . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+        . 2 c 2 e e e e e e e b c 4 2 2 
+        . 2 2 e b b e b b b e e b 4 2 2 
+        . 2 e b b b e b b b b e 2 2 2 2 
+        . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+        . e e e e e e f e e e f e 2 d d 
+        . e e e e e e f e e f e e e 2 d 
+        . e e e e e e f f f e e e e e e 
+        . e f f f f e e e e f f f e e e 
+        . . f f f f f e e f f f f f e . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+})
+let truck: Sprite = null
+tiles.setCurrentTilemap(tilemap`nivel1`)
+truck = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+    . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+    . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+    . 2 c 2 e e e e e e e b c 4 2 2 
+    . 2 2 e b b e b b b e e b 4 2 2 
+    . 2 e b b b e b b b b e 2 2 2 2 
+    . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+    . e e e e e e f e e e f e 2 d d 
+    . e e e e e e f e e f e e e 2 d 
+    . e e e e e e f f f e e e e e e 
+    . e f f f f e e e e f f f e e e 
+    . . f f f f f e e f f f f f e . 
+    . . . f f f . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+truck.setPosition(10, 10)
+truck.setStayInScreen(true)
+truck.ay = 150
+scene.cameraFollowSprite(truck)
+controller.moveSprite(truck, 50, 0)
+game.onUpdate(function () {
+    if (controller.A.isPressed() && truck.isHittingTile(CollisionDirection.Bottom)) {
+        truck.vy = -75
+    }
+    console.logValue("x", truck.x)
+    console.logValue("y", truck.x)
+    console.logValue("anchura de la pantalla", scene.screenWidth())
+    console.logValue("altura de la pantalla", scene.screenHeight())
+})
